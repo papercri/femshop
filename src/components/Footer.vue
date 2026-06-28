ea71a7d1d2554fa910e4105ad4186ceb55e5b6b9
Your NameCristiana <papercri@gmail.com>
2026-06-28 16:49:38 +0200

    credits

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -27,15 +27,28 @@
                     <h3 class="mb-2 font-semibold ">Contact</h3>
                     <div class="flex flex-col">
                         <div class="flex items-center gap-2 text-start">
+                            <span>Cris</span>
                             <a href="https://www.linkedin.com/in/cristianasollini/" target="_blank"><i class="fa-brands fa-linkedin text-lg cursor-pointer hover:opacity-70"></i></a>
                             <a href="https://github.com/papercri" target="_blank"><i class="fa-brands fa-github text-lg cursor-pointer hover:opacity-70"></i></a>
                             <a href="https://frontend-ux.website/" target="_blank"><i class="fa-solid fa-globe text-lg cursor-pointer hover:opacity-70"></i></a>
                         </div>
-                      
+                         <div class="flex items-center gap-2 text-start">
+                            <span>Daniela</span>
+                            <a href="https://www.linkedin.com/in/dperezro/" target="_blank"><i class="fa-brands fa-linkedin text-lg cursor-pointer hover:opacity-70"></i></a>
+                            <a href="https://github.com/dperezca" target="_blank"><i class="fa-brands fa-github text-lg cursor-pointer hover:opacity-70"></i></a>
+                        </div> 
+                         <div class="flex items-center gap-2 text-start">
+                            <span>Jocelyn</span>
+                            <a href="https://www.linkedin.com/in/jocelyn-castro-flores" target="_blank"><i class="fa-brands fa-linkedin text-lg cursor-pointer hover:opacity-70"></i></a>
+                            <a href="https://github.com/Jocelyn212" target="_blank"><i class="fa-brands fa-github text-lg cursor-pointer hover:opacity-70"></i></a>
+                        </div> 
                     </div>
                 </div>
                 <div class="flex flex-col md:justify-center justify-start items-center p-4">
-           
+                    <!-- <a href="https://github.com/Jocelyn212/ProyectoFinal" target="_blank" class="flex flex-col items-center justify-center hover:opacity-70">
+                        <div class="flex items-center text-center"><i class="fa-brands fa-github text-5xl cursor-pointer"></i></div>
+                        <h3 class="text-center text-sm">Check our repository!</h3>
+                    </a> -->
                 </div>
                 <p class="sm:col-span-2 md:col-span-4 text-center !text-xs pt-5  border-t border-lila-primary"> FemShop © {{ new Date().getFullYear() }}.  All rights reserved.</p>
             </div>
